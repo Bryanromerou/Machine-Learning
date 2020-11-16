746777cf0a4a0b60154927c2fa1241f1bacd0070
Bryan Romero <bryanromerou@gmail.com>
2020-11-15 21:48:21 -0800

    Was able to bring in data and append an element from csv file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 from pprint import pprint
 
+my_data = pd.read_csv("data.csv")
 url = "https://realtor.p.rapidapi.com/locations/auto-complete"
 
 querystring = {"input":"new york"}
@@ -11,6 +12,16 @@ headers = {
     'x-rapidapi-host': "realtor.p.rapidapi.com"
     }
 
+new_entry = {
+    "Address": "1317 Prado St.",
+    "Price": 500000,
+    "Bedrooms": 3,
+    "Baths": 2,
+    "Schools": 2,
+}
+print(my_data.append(new_entry, ignore_index=True))
+my_data = my_data.append(new_entry, ignore_index=True)
 # response = requests.request("GET", url, headers=headers, params=querystring)
-pprint(response.json())
+# pprint(response.json())
+print(my_data)
 # print(response.text)
